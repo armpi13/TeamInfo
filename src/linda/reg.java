b2e4f0b4ce103c4cbedb666b18c442f71499beb8
Armpi <armpingkela@gmai.com>
2023-12-04 17:07:26 +0200

    5:04

--- src/linda/reg.java
+++ src/linda/reg.java
@@ -386,7 +386,7 @@ try {
         txtname.setText("");
         txtyear.setText("");
         txtid.setText("");
-        //otinanai
+        
     }//GEN-LAST:event_jButton3ActionPerformed
                                                                                             //ABOUT 
     private void jButton6ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton6ActionPerformed
